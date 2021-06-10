192b99b546b05c61f13c3234055ec05a205667b5
tychoverstraete <tycho.verstraete@student.ehb.be>
2021-06-10 03:56:04 +0200

    get future posts + delete posts

--- infludash-api/Controllers/PostsController.cs
+++ infludash-api/Controllers/PostsController.cs
@@ -1,6 +1,8 @@
 ﻿using infludash_api.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -38,6 +40,48 @@ namespace infludash_api.Controllers
 
                 return Ok(context.posts.ToList().Where(s => s.email.Equals(email)));
             }
+        }
+
+        // POST api/posts/future
+        [HttpPost("future")]
+        [Authorize]
+        public IActionResult FuturePosts()
+        {
+            using (StreamReader stream = new StreamReader(HttpContext.Request.Body))
+            {
+                Task<string> body = stream.ReadToEndAsync();
+                if (body.Result == "")
+                {
+                    return BadRequest("You must specify the user email");
+                }
+                dynamic jsonBody = JObject.Parse(body.Result);
+                string email = jsonBody.userEmail;
+
+                return Ok(context.posts.ToList().Where(s => s.email.Equals(email) && s.scheduled > DateTime.Now));
+            }
+        }
+
+        // DELETE api/posts
+        [HttpDelete("{id}")]
+        [Authorize]
+        public IActionResult FuturePosts(int id)
+        {
+            try
+            {
+                var post = context.posts.Where(p => p.id == id).First();
+                // delete job linked with post
+                var cmdTextPost = "DELETE FROM job WHERE id=@id";
+                var idParam = new MySqlParameter("@id", post.postId);
+                context.Database.ExecuteSqlRaw(cmdTextPost, idParam);
+                context.posts.Remove(post);
+                context.SaveChanges();
+                return Ok(post);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
         }
     }
 }
